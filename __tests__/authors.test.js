915d0334f637a22cf9ef359d16ee946e23512c72
Hunter Czarapata <czarapata.hunter@gmail.com>
2022-11-08 09:26:09 -0800

    Redid the authors test, controller, and model to correctly get all authors

---
+++ __tests__/authors.test.js
@@ -0,0 +1,20 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('authors routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('GET owners should return list of authors', async () => {
+    const resp = await request(app).get('/authors');
+    expect(resp.status).toBe(200);
+    expect(resp.body.length).toBe(3);
+    expect(resp.body[0]).toEqual({
+      id: expect.any(String),
+      name: expect.any(String),
+    });
+  });
+});
